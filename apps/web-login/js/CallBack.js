a30eff937a383f29bff0e28140fe0856a457c679
sung <reputable4126@gmail.com>
2023-03-25 01:43:51 +0900

    docs & rename (ip 상대주소 변경, 파일 위치와 Login.html->index.html, 주석제거 )

---
+++ apps/web-login/js/CallBack.js
@@ -0,0 +1,19 @@
+const naverOuath_ID = "Hw7RwBFOB7UjSufSEEjy";
+const callBack_URL = new URL(document.location.href);
+const index_URL = callBack_URL.origin;
+var naver_id_login = new naver_id_login(naverOuath_ID, index_URL);
+alert(index_URL);
+// 네이버 사용자 프로필 조회
+naver_id_login.get_naver_userprofile("naverSignInCallback()");
+// 네이버 사용자 프로필 조회 이후 프로필 정보를 처리할 callback function
+function naverSignInCallback() {
+  alert(naver_id_login.getProfileData("email"));
+  alert(naver_id_login.getProfileData("nickname"));
+  alert(naver_id_login.getProfileData("age"));
+  alert("로그인 성공");
+}
+
+// 로그아웃 구현
+function logOut() {
+  window.open(`https://nid.naver.com/nidlogin.logout`);
+}
