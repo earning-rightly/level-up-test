a243a5501a50ebf34ae32ec48f5f4a986d784075
sung <reputable4126@gmail.com>
2023-04-07 11:21:56 +0900

    <test> NaverCallback-Page.js애서 userprofile 정보를 못가져왔다
    refactory를 하는 중에 userprfile에 원래있어야 할 정보가 undefine
    으로 바뀐것을 확인했다. 그래서 원인 찾던중 NaverCallBack-Page.js가
    module로 바뀐 다음이라고 생각이 들었고 moudule을 취소해보니 원래대로
    작동하게 되었다.

--- apps/web-login/js/NaverCallBack-Page.js
+++ apps/web-login/js/NaverCallBack-Page.js
@@ -1,7 +1,17 @@
-import { NAVER_CLIENT_ID, NAVER_OAUTH_SECRETE } from "./moudle/constants.js";
-import { cookieName } from "./moudle/constants.js";
-import { indexURL, callbackURL, pathname } from "./moudle/url_list.js";
-import { naverLogOut } from "./moudle/Logout.js";
+// import { NAVER_CLIENT_ID, NAVER_OAUTH_SECRETE } from "./moudle/constants.js";
+// import { cookieName } from "./moudle/constants.js";
+// import { indexURL, callbackURL, pathname } from "./moudle/url_list.js";
+// import { naverLogOut } from "./moudle/Logout.js";
+const NAVER_CLIENT_ID = "Hw7RwBFOB7UjSufSEEjy"; // 상수는 따로 .js 파일에 넣어서 관리하자
+const cookieName = "userProfile";
+
+let indexURL = new URL(document.location.href);
+let pathname = indexURL.pathname; // url-path 부분
+
+let callbackURL =
+  indexURL.origin + // scheme와 hosts 부분
+  pathname.substring(0, pathname.lastIndexOf("/")) +
+  "/CallBack.html";
 
 let naverLogOutBtn = document.getElementById("naver-logout-btn");
 
@@ -11,11 +21,15 @@ let naverLogOutBtn = document.getElementById("naver-logout-btn");
 
 var userProfile = {};
 var naverIdLogin = new naver_id_login(NAVER_CLIENT_ID, callbackURL);
-console.log(callbackURL);
-naverSignInCallback();
 
+// naverSignInCallback();
+
+naverIdLogin.get_naver_userprofile("naverSignInCallback()"); // 실행이 안됨
 function naverSignInCallback() {
   // userProfile 객체를 JSON 형식으로 변환하여 쿠키에 저장
+  userProfile = {
+    token: naverIdLogin.oauthParams.access_token,
+  };
   if (UserProfileInputCookies()) {
     CloseCallbackPage();
   } else {
@@ -23,8 +37,16 @@ function naverSignInCallback() {
   }
 }
 
-console.log(indexURL);
+// function naverSignInCallback() {
+//   // userProfile 객체를 JSON 형식으로 변환하여 쿠키에 저장
+//   alert(naverIdLogin.getProfileData("email"));
+//   alert(naverIdLogin.getProfileData("nickname"));
+//   alert(naverIdLogin.getProfileData("age"));
+// }
+console.log(naverIdLogin);
+
 function UserProfileInputCookies() {
+  console.log("UserProfileInputCookies");
   userProfile = {
     token: naverIdLogin.oauthParams.access_token,
     email: naverIdLogin.getProfileData("email"),
@@ -36,7 +58,7 @@ function UserProfileInputCookies() {
     return false;
   } else {
     document.cookie = cookieName + "=" + JSON.stringify(userProfile);
-    console.log(userProfile);
+    console.log("userProfile", userProfile);
     return true;
   }
 }
@@ -44,7 +66,7 @@ function UserProfileInputCookies() {
 function CloseCallbackPage() {
   console.log("close funtion");
   window.opener.postMessage(
-    "close",
+    "use Naver Oauth",
     indexURL + // scheme와 hosts 부분
       pathname.substring(0, pathname.lastIndexOf("/")) +
       "/login.html"
@@ -52,16 +74,8 @@ function CloseCallbackPage() {
   // window.close();
 }
 
-window.addEventListener("message", function (event) {
-  // 이벤트를 처리하는 코드 작성
-
-  console.log(event.data); // 전달된 데이터 출력
-  if (event.data === "use Naver Oauth") {
-  }
-});
-
 function LogOut() {
-  naverLogOut(userProfile);
+  window.open(`https://nid.naver.com/nidlogin.logout`);
 }
 
 naverLogOutBtn.addEventListener("click", LogOut);
